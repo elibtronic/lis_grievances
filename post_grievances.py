901a7cdedbb19fe9f987a9d32df6ee71d9f58d46
grief <grief@lisgrievances.com>
2016-02-29 09:42:07 -0500

    Only connects via API if there are things to post

--- post_grievances.py
+++ post_grievances.py
@@ -7,27 +7,25 @@ from settings import *
 
 
 # Try to connect to Twitter first
-try:
-	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-	auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-	api = tweepy.API(auth)
-except:
-	print("Error Connecting to Twitter")
-	exit
 
 try:
 	grieve_file = open(GTPATH,"r+")
 	g = grieve_file.readline()
-	rest_g = grieve_file.read()
-	
-	grieve_file.seek(0)
-	grieve_file.truncate()
-	grieve_file.write(rest_g)
-	grieve_file.close()
-	
 	if g != "":
 		print(g)
+		rest_g = grieve_file.read()
+	
+		grieve_file.seek(0)
+		grieve_file.truncate()
+		grieve_file.write(rest_g)
+		grieve_file.close()
+	
+		#Only connect if there is something to post
+		auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+		auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+		api = tweepy.API(auth)
 		api.update_status(g)
+
 		print("Done")
 	else:
 		print("Grievance did not post")
