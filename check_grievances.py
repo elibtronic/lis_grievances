ce61539c1503daf9ae92347cd31bb51b5845e0a8
grief <grief@lisgrievances.com>
2017-11-16 14:52:37 -0500

    Revert "reads emojis (unicodes) now"
    This reverts commit 4a9661cedec99e304d0542a80248fa9c85a04add.

--- check_grievances.py
+++ check_grievances.py
@@ -17,17 +17,16 @@ try:
 	http = urllib3.PoolManager()
 
 	for m in dms:
-
-		if len(m.text.encode('utf-8')) > 280:
-			mres = api.send_direct_message(m.sender.screen_name, text="Oops. Your grievance is longer than 280 characters, please try again.")
+		if len(m.text) > 280:
+			mres = api.send_direct_message(m.sender.screen_name, text="Oops. Your grievance is longer than 140 characters, please try again.")
 			print("LONG "+m.text)
 			r = http.request('GET',GFORM_URL+quote("LONG "+m.text))
 		else:
 			mres = api.send_direct_message(m.sender.screen_name, text="Thanks, your grievance has been queued for approval.")
-			print(m.text.encode('utf-8'))
+			print(m.text)
 			#Grievances are tabulated annoymously in a Google Form where they are eventually posted
 			#This part of the process will be automated in future version
-			r = http.request('GET',GFORM_URL+quote(m.text.encode('utf-8')))
+			r = http.request('GET',GFORM_URL+quote(m.text))
 
 		#Destroy the message received and the response to it
 		api.destroy_direct_message(m.id)
