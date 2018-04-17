74084712255b8f0ddcce1108fd316cfc7a6d0eb5
grief <grief@lisgrievances.com>
2018-04-17 16:50:19 -0400

    Check works with Unicode now

--- check_grievances.py
+++ check_grievances.py
@@ -16,14 +16,19 @@ try:
 	dms = api.direct_messages(full_text=True)
 	http = urllib3.PoolManager()
 
+	#Posts come in as unicode characters, prints ascii safe version to the screen
+
 	for m in dms:
+		#for screen debugging
+		m_clear = m.text.encode("utf-8")
+
 		if len(m.text) > 280:
 			mres = api.send_direct_message(m.sender.screen_name, text="Oops. Your grievance is longer than 280 characters, please try again.")
-			print("LONG "+m.text)
+			print("LONG "+m_clear)
 			r = http.request('GET',GFORM_URL+quote("LONG "+m.text))
 		else:
 			mres = api.send_direct_message(m.sender.screen_name, text="Thanks, your grievance has been queued for approval. More details here: http://lisgrievances.com/about.html")
-			print(m.text)
+			print(m_clear)
 			#Grievances are tabulated annoymously in a Google Form where they are eventually posted
 			#This part of the process will be automated in future version
 			r = http.request('GET',GFORM_URL+quote(m.text))
@@ -33,7 +38,8 @@ try:
 		api.destroy_direct_message(mres.id)
 	print("Done Checking")
 
-except:
+except Exception as e:
+	print("Exception: ",e)
 	print("Could not connect to Twitter to check")
 
 
@@ -45,8 +51,8 @@ try:
 	dms = api.direct_messages()
 	# The nuclear option, will delete all DM's to the bot.
 	for m in dms:
+		print("pass")
 		api.destroy_direct_message(m.id)
 	print("Done Borking")
 except:
 	print("Could not connect to twitter to bork")
-
